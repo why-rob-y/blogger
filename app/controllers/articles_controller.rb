0fcbe0b15379602224958bdece84bf24ac1a8a19
Rob Y <why.rob.y@gmail.com>
2015-02-13 11:41:48 -0500

    Sorcery authentication complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+before_filter :require_login, only: [:new, :create, :edit, :update, :destroy]
+
 include ArticlesHelper
 def index
   @articles = Article.all
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
 root to: 'articles#index'
 resources :articles do
   resources :comments
+
 end
 
-
+  resources :authors
 resources :tags
+
+
+resources :author_sessions, only: [ :new, :create, :destroy ]
+
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
+
 end
